3be3981d318eff963615a6eaec24363519c1d2d8
Edwin Paul <68837346+sanedealer@users.noreply.github.com>
2024-10-07 21:09:30 +0400

    Update polling.js

--- polling.js
+++ polling.js
@@ -1,5 +1,5 @@
 // This is the code for your Web Worker.
 setInterval(() => {
-    // Send a message back to the main thread every 5 seconds
+    // Send a message back to the main thread every 3 seconds
     self.postMessage({ message: 'Ping', timestamp: Date.now() });
 }, 3000);
